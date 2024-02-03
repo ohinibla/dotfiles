cd2b76e350ee54e9d52135195feb68027a8c499c
babak <ohinibla@gmail.com>
2024-02-03 10:40:44 +0330

    added ssr plugin for search and replace

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -52,6 +52,28 @@ lvim.plugins = {
       }
     end
   },
+  {
+    "cshuaimin/ssr.nvim",
+    lazy = true,
+    -- Calling setup is optional.
+    config = function()
+      require("ssr").setup {
+        border = "rounded",
+        min_width = 50,
+        min_height = 5,
+        max_width = 120,
+        max_height = 25,
+        adjust_window = true,
+        keymaps = {
+          close = "q",
+          next_match = "n",
+          prev_match = "N",
+          replace_confirm = "<cr>",
+          replace_all = "<leader><cr>",
+        },
+      }
+    end
+  },
   { "shortcuts/no-neck-pain.nvim", version = "*" },
   { "ellisonleao/glow.nvim",       config = true, cmd = "Glow" },
   {
--- lvim/lua/user/which_key.lua
+++ lvim/lua/user/which_key.lua
@@ -15,6 +15,10 @@ lvim.builtin.which_key.mappings["s"]["T"] = {
   "<CMD>Telescope tags<CR>", "Search Workspace tags"
 }
 
+lvim.builtin.which_key.vmappings["s"] = {
+  "<cmd>lua require('ssr').open()<CR>", "search and replace"
+}
+
 lvim.builtin.which_key.mappings["C"] = {
   name = "Python",
   c = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env" },
