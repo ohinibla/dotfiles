3eb4c897e48ff3344dbda0387eb5b0aa79b2db9f
ohinibla <ohinibla@gmail.com>
2024-02-13 16:28:12 +0330

    fixed macro recording message not showing whith Noice plugin
    since Noice hides cmdline, using lualine to show macro recording message.

--- lvim/lua/user/autocommands.lua
+++ lvim/lua/user/autocommands.lua
@@ -12,3 +12,39 @@
 --   end,
 -- })
 --
+
+
+-- refreshing lualine manually to show recording message: https://www.reddit.com/r/neovim/comments/xy0tu1/comment/irfegvd/?utm_source=share&utm_medium=web2x&context=3
+-- there's going to be a delay in Lualine as by default Lualine only refreshes every second.
+-- So what I do is I use Lualine's in-built refresh function to force an update when I enter
+-- or exit recording a macro:
+local lualine = require("lualine")
+
+vim.api.nvim_create_autocmd("RecordingEnter", {
+  callback = function()
+    lualine.refresh({
+      place = { "statusline" },
+    })
+  end,
+})
+
+vim.api.nvim_create_autocmd("RecordingLeave", {
+  callback = function()
+    -- This is going to seem really weird!
+    -- Instead of just calling refresh we need to wait a moment because of the nature of
+    -- `vim.fn.reg_recording`. If we tell lualine to refresh right now it actually will
+    -- still show a recording occuring because `vim.fn.reg_recording` hasn't emptied yet.
+    -- So what we need to do is wait a tiny amount of time (in this instance 50 ms) to
+    -- ensure `vim.fn.reg_recording` is purged before asking lualine to refresh.
+    local timer = vim.loop.new_timer()
+    timer:start(
+      50,
+      0,
+      vim.schedule_wrap(function()
+        lualine.refresh({
+          place = { "statusline" },
+        })
+      end)
+    )
+  end,
+})
--- lvim/lua/user/lualine.lua
+++ lvim/lua/user/lualine.lua
@@ -1,17 +1,16 @@
 -- add selctioncount to lualine
+local show_selection_count = require("user.functions").show_selection_count
+local show_macro_recording = require("user.functions").show_macro_recording
 local components = require("lvim.core.lualine.components")
 lvim.builtin.lualine.sections.lualine_y = {
   components.location,
   {
-    function()
-      local starts = vim.fn.line("v")
-      local ends = vim.fn.line(".")
-      local count = starts <= ends and ends - starts + 1 or starts - ends + 1
-      local wc = vim.fn.wordcount()
-      return count .. ":" .. wc["visual_chars"]
-    end,
+    show_selection_count,
     cond = function()
-      return vim.fn.mode():find("[Vv]") ~= nil
+      return vim.fn.mode():find("[vv]") ~= nil
     end
+  },
+  {
+    show_macro_recording
   }
 }
