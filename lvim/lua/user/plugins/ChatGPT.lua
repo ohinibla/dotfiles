cccd1d60d4c0bbc847ea0f2d60ec0daf28ea61a4
ohinibla <ohinibla@gmail.com>
2024-02-14 16:49:40 +0330

    added ChatGPT plugin

---
+++ lvim/lua/user/plugins/ChatGPT.lua
@@ -0,0 +1,20 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("chatgpt").setup(
+      {
+        api_key_cmd = "op read op://MazBob/auth0.openai.com/63xz76vyggvctegbph2b4o66hi --no-newline",
+        extra_curl_params = {
+          "--socks5",
+          "127.0.0.1:10808"
+        }
+      })
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "folke/trouble.nvim",
+    "nvim-telescope/telescope.nvim"
+  }
+}
