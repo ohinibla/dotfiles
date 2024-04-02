709f5a44e2643b8ad2c01a9c42bdee7a5239d9cf
ohinibla <ohinibla@gmail.com>
2024-04-02 20:48:47 +0330

    minor change

--- hypr/scripts/autostart.sh
+++ hypr/scripts/autostart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-. ~/.bashrc
 exec /usr/local/bin/launch.sh &
 
 swww query || swww init & 
 pypr --config /home/babak/.config/dotfiles/hypr/pyprland.toml &
 exec ~/.config/dotfiles/hypr/scripts/suspend.sh &
 dunst & 
+~/.local/bin/nekoray
 1password
 # /usr/libexec/polkit-gnome-autentication-agent-1 &
