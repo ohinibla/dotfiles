05cde60fc0d23d7653494e879d52be25a7201319
ohinibla <ohinibla@gmail.com>
2024-07-31 23:17:11 +0330

    ✨ (colorschemes.lua): add 'olimorris/onedarkpro.nvim' colorscheme plugin with high priority to ensure it loads first

--- lvim/lua/user/colorschemes.lua
+++ lvim/lua/user/colorschemes.lua
@@ -1,6 +1,10 @@
 for _, plugin in ipairs({
 	{ "rebelot/kanagawa.nvim" },
 	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+	{
+		"olimorris/onedarkpro.nvim",
+		priority = 1000, -- Ensure it loads first
+	},
 }) do
 	lvim.plugins[#lvim.plugins + 1] = plugin
 end
